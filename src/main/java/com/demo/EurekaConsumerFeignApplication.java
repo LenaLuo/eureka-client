8896c363648dd41094240e6cae816e7813a59b21
Lena Luo <lena.luo@sap.com>
2017-12-16 19:52:12 +0800

    commit

---
+++ src/main/java/com/demo/EurekaConsumerFeignApplication.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerFeignApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(EurekaConsumerFeignApplication.class, args);
+		new SpringApplicationBuilder(EurekaConsumerFeignApplication.class).web(true).run(args);
+	}
+}
